34bbfe33b1af4dccbd07eb2056e6181bca7ad085
zhanglq <zhanglq@bywin.cn>
2023-05-15 12:06:48 +0800

    add emotion-recognition

--- scripts/env_test/env_print.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-print(torch.__version__, torchaudio.__version__, torch.cuda.is_available(),device)
